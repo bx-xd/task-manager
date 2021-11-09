e90ca8b8199d81249179b7e9154fef57a26f47ce
Antoine Brique <antoine.brique@gmail.com>
2021-11-09 13:04:50 +0100

    complete read and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show'
 end
